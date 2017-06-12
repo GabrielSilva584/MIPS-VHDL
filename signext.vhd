d1b8741f0ede74ef7d43d7bf12a706ecd622ea26
Gabriel Martins da Silva <gms.1997@hotmail.com>
2017-06-12 20:15:55 -0300

    Sign Extender
    Sainu Ecstendu

--- signext.vhd
+++ signext.vhd
@@ -4,18 +4,18 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity signext is -- sign extender
 	generic(
-		CONSTANT bits_c: integer; --32
-		CONSTANT adress_c: integer --16
+		CONSTANT bits_c		: integer; --32
+		CONSTANT adress_c		: integer --16
 	);
 	
 	port(
-		a	: in	STD_LOGIC_VECTOR(adress_c-1 downto 0);
-		y	: out	STD_LOGIC_VECTOR(bits_c-1 downto 0)
+		i_a	: in	STD_LOGIC_VECTOR(adress_c-1 downto 0);
+		o_y	: out	STD_LOGIC_VECTOR(bits_c-1 downto 0)
 	);
 end;
 
 architecture behave of signext is
 begin
-	y(adress_c-1 downto 0)        <= a;
-	y(bits_c-1 downto adress_c)   <= (others => a(adress_c-1));
+	o_y(adress_c-1 downto 0)        <= i_a;
+	o_y(bits_c-1 downto adress_c)   <= (others => i_a(adress_c-1));
 end;
