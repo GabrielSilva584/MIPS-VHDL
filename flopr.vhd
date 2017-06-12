754c6141e59190c54b0fe5637223793009cb803f
Newairs <gabriel_arantes18@hotmail.com>
2017-06-12 20:06:50 -0300

    Upei na marra
    Não minto

--- flopr.vhd
+++ flopr.vhd
@@ -3,24 +3,26 @@ use IEEE.STD_LOGIC_1164.all;
 
 
 entity flopr is -- flip-flop with synchronous reset
+	
 	generic(
-		width		: integer
+		width_g		: INTEGER
 	);
+	
 	port(
-		clk, reset	: in		STD_LOGIC;
-		d			: in		STD_LOGIC_VECTOR(width-1 downto 0);
-		q			: out		STD_LOGIC_VECTOR(width-1 downto 0)
+		i_clk, i_reset	: in		STD_LOGIC;
+		i_d			   : in		STD_LOGIC_VECTOR(width_g-1 downto 0);
+		o_q			   : out		STD_LOGIC_VECTOR(width_g-1 downto 0)
 	);
 end;
 
 architecture synchronous of flopr is
 begin
 	
-	risingEdge: PROCESS(clk, reset)
+	risingEdge: PROCESS(i_clk, i_reset)
 	BEGIN
-		IF (clk'EVENT AND clk'LAST_VALUE = '0') THEN
-			IF reset = '1' THEN q <= (others => '0');
-			ELSE q <= d;
+		IF (i_clk'EVENT AND clk'LAST_VALUE = '0') THEN
+			IF i_reset = '1' THEN o_q <= (others => '0');
+			ELSE o_q <= i_d;
 			END IF;
 		END IF;
 	END PROCESS;
